01d274d2eca25adb901fe7d62f84442ed73bb494
Sp0146001 <sp0146001@gmail.com>
2025-10-25 14:55:48 +0300

    the initial code of the Pythagorean triples program

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
-
+bool isPyth(unsigned a, unsigned b, unsigned c){
+	bool p = ;
+	bool p =p ||  ;
+	bool p =p ||  ;
+	return p;
+}
 int main(){
 	using u_t = unsigned;
-	u_t a = 0;
+	u_t a = b = c = 0;
 	size_t count = 0;
 	while(std::cin >> a){
-		count++;
+		count+=isPyth(a,b,c) ? 1 : 0;
 	}
 	if(std::cin.eof()){
 		std::cout << count << "\n";
