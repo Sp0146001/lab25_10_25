39a7fd985923fff65f438a6ea6937684e072f83a
Sp0146001 <sp0146001@gmail.com>
2025-10-25 15:08:48 +0300

    finished code of Pythagorean triples without checking of numbers of triangle

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
 bool isPyth(unsigned a, unsigned b, unsigned c){
-	bool p = ;
-	bool p =p ||  ;
-	bool p =p ||  ;
+	bool p = a*a ==(b*b + c*c);
+	p = p || (b*b == c*c + a*a);
+	p = p || (c*c == a*a + b*b);
 	return p;
 }
 int main(){
 	using u_t = unsigned;
-	u_t a = b = c = 0;
+	u_t a = 0, b = 0, c = 0;
+	std::cin >> c >> b;
 	size_t count = 0;
 	while(std::cin >> a){
 		count+=isPyth(a,b,c) ? 1 : 0;
+		c = b;
+		b = a;
 	}
 	if(std::cin.eof()){
 		std::cout << count << "\n";
