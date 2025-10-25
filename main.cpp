588e43494a9394c67d9ad39f73afc753b62a97a9
Sp0146001 <sp0146001@gmail.com>
2025-10-25 20:54:16 +0300

    final version, completed &work

--- main.cpp
+++ main.cpp
@@ -5,31 +5,31 @@ u_t max_u(){
 	using namespace std;
 	using u_limit = numeric_limits< u_t >;
 	return u_limit::max();
+}
 u_t min_u(){
         using namespace std;
         using u_limit = numeric_limits< u_t >;
         return u_limit::min();
-
+}
 bool ver_ovfl(unsigned a, unsigned b, unsigned c){
-	bool p1 = (min_u/a) < a && a< (max_u/a);
-	p1 = p1 && ((min_u/b) < b) && (b < (max_u/b));
-	p1 = p1 && ((min_u/c) < c) && (c < (max_u/c));
+	bool p1 = (min_u() / a) < a && a < (max_u() / a);
+	p1 = p1 && ((min_u() / b) < b) && (b < (max_u() / b));
+	p1 = p1 && ((min_u() / c) < c) && (c < (max_u() / c));
 	if(p1){
-		bool p2 = (((min_u-a*a) < b*b) && (b*b < (max_u-a*a)));
-		p2 = (((min_u-c*c) < b*b) && (b*b < (max_u-c*c)));
-		p2 = (((min_u-a*a) < c*c) && (c*c < (max_u-a*a)));
+		p1 =p1 && ((max_u() - a*a) > b*b) && ((max_u() - b*b) > c*c);
+	        p1 = p1 && ((max_u() - a*a) > c*c) && ((max_u() - c*c) > b*b);
+	        p1 = p1 && ((max_u() - b*b) > a*a) && ((max_u() - c*c) > a*a);
 	}
-	return (p1 && p2);
+	return p1;
 }
 
 bool isPyth(unsigned a, unsigned b, unsigned c){
-	bool p = (ver_ovfl(a,b,c) && (a*a ==(b*b + c*c)));
-	p = p || (ver_ovfl(b,a,c) && (b*b ==(a*a + c*c)));
-	p = p || (ver_ovfl(c,a,b) && (c*c ==(b*b + a*a)));
+	bool p = a*a ==(b*b + c*c);
+	p = p || (b*b == c*c + a*a);
+	p = p || (c*c == a*a + b*b);
 	return p;
 }
 int main(){
-	using u_t = unsigned;
 	u_t a = 0, b = 0, c = 0;
 	std::cin >> c >> b;
 	size_t count = 0;
@@ -39,8 +39,10 @@ int main(){
 			c = b;
 			b = a;
 		}
-		std::cerr << "Overflowing was detected\n";
-		return 2;
+		else {
+			std::cerr << "Overflowing was detected\n";
+			return 2;
+		}
 	}
 	if(std::cin.eof()){
 		std::cout << count << "\n";
