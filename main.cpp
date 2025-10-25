83988154af7b71180db782c424bdfa2e608139c6
Sp0146001 <sp0146001@gmail.com>
2025-10-25 14:25:45 +0300

    added input verification(EOF and FAIL)

--- main.cpp
+++ main.cpp
@@ -5,5 +5,13 @@ int main(){
 	using u_t = unsigned;
 	u_t a = 0;
 	std::cin >> a;
+	
+	if(std::cin.eof()){
+		std::cout << 1 << "\n";
+	}
+	else if(std::cin.fail()){
+		std::cerr << "Input end\n";
+		return 1;
+	}
 	return 0;
 }
