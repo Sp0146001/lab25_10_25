0b20dfb38eae72b4b3103335f5cd3bfc17f2293c
Sp0146001 <sp0146001@gmail.com>
2025-10-25 20:03:06 +0300

    pre-final version of task with verifying overflowing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-bool verify_overflowing(unsigned a, unsigned b, unsigned c){
-	
+#include <limits>
+using u_t = unsigned;
+u_t max_u(){
+	using namespace std;
+	using u_limit = numeric_limits< u_t >;
+	return u_limit::max();
+u_t min_u(){
+        using namespace std;
+        using u_limit = numeric_limits< u_t >;
+        return u_limit::min();
+
+bool ver_ovfl(unsigned a, unsigned b, unsigned c){
+	bool p1 = (min_u/a) < a && a< (max_u/a);
+	p1 = p1 && ((min_u/b) < b) && (b < (max_u/b));
+	p1 = p1 && ((min_u/c) < c) && (c < (max_u/c));
+	if(p1){
+		bool p2 = (((min_u-a*a) < b*b) && (b*b < (max_u-a*a)));
+		p2 = (((min_u-c*c) < b*b) && (b*b < (max_u-c*c)));
+		p2 = (((min_u-a*a) < c*c) && (c*c < (max_u-a*a)));
+	}
+	return (p1 && p2);
 }
+
 bool isPyth(unsigned a, unsigned b, unsigned c){
-	bool p = a*a ==(b*b + c*c);
-	p = p || (b*b == c*c + a*a);
-	p = p || (c*c == a*a + b*b);
+	bool p = (ver_ovfl(a,b,c) && (a*a ==(b*b + c*c)));
+	p = p || (ver_ovfl(b,a,c) && (b*b ==(a*a + c*c)));
+	p = p || (ver_ovfl(c,a,b) && (c*c ==(b*b + a*a)));
 	return p;
 }
 int main(){
@@ -14,9 +34,13 @@ int main(){
 	std::cin >> c >> b;
 	size_t count = 0;
 	while(std::cin >> a){
-		count+=isPyth(a,b,c) ? 1 : 0;
-		c = b;
-		b = a;
+		if(ver_ovfl(a,b,c)){
+			count+=isPyth(a,b,c) ? 1 : 0;
+			c = b;
+			b = a;
+		}
+		std::cerr << "Overflowing was detected\n";
+		return 2;
 	}
 	if(std::cin.eof()){
 		std::cout << count << "\n";
